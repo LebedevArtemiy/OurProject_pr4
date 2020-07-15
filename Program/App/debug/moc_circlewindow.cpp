70f2249b4b3b4d8c7c8deb1b2878db2299094a9d
LebedevArtemiy <67901623+LebedevArtemiy@users.noreply.github.com>
2020-07-15 21:30:20 +0300

    without trapezoid2

--- Program/App/debug/moc_circlewindow.cpp
+++ Program/App/debug/moc_circlewindow.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'circlewindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "../circlewindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'circlewindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.2. It"
+#error "This file was generated using the moc from 5.15.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -98,7 +99,7 @@ void CircleWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
 }
 
 QT_INIT_METAOBJECT const QMetaObject CircleWindow::staticMetaObject = { {
-    &QWidget::staticMetaObject,
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
     qt_meta_stringdata_CircleWindow.data,
     qt_meta_data_CircleWindow,
     qt_static_metacall,
